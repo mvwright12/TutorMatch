4e5211d790d2fc7e83353a8483f70301b2384866
mvwright12 <mvwright12@gmail.com>
2020-03-04 10:18:28 -0500

    Initial Commit

---
+++ TutorMatch.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using TutorMatch.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TutorMatch.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
