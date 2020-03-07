9d69066a6ae8400e5be3b52397ccd4db2a1e558c
mvwright12 <mvwright12@gmail.com>
2020-03-07 12:46:09 -0500

    Add user cards to Angular

---
+++ TutorMatch-SPA/src/app/_models/user.ts
@@ -0,0 +1,19 @@
+import { Photo } from './photo';
+
+export interface User {
+    id: number;
+    username: string;
+    studentTutor: string;
+    gender: string;
+    year: string;
+    subjects: string;
+    availability: string;
+    rateRange: string;
+    created: Date;
+    lastActive: Date;
+    photoUrl: string;
+    city: string;
+    state: string;
+    introduction?: string;
+    photos?: Photo[];
+}
