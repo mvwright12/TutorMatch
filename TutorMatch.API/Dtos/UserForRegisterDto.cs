0e8ff33a839fb454962320108f6ef0ea58c5f7cb
mvwright12 <mvwright12@gmail.com>
2020-03-09 11:52:30 -0400

    Add reactive form to register new users

--- TutorMatch.API/Dtos/UserForRegisterDto.cs
+++ TutorMatch.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace TutorMatch.API.Dtos
@@ -10,5 +11,24 @@ namespace TutorMatch.API.Dtos
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
         public string Password { get; set; }
+        [Required] 
+        public string StudentTutor { get; set; }
+        [Required] 
+        public string Gender { get; set; }
+        [Required] 
+        public string Year { get; set; }
+        [Required] 
+        public string RateRange { get; set; }
+        [Required] 
+        public string City { get; set; }
+        [Required] 
+        public string State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
